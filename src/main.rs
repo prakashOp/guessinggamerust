e97a90f7b46c9fd8bfd17a18791d5cebaa5ed6cd
prakashOp <prakash127001@gmail.com>
2024-12-12 10:50:06 +0530

    setup random number generator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+use rand::Rng;
+use colored::*;
+use std::cmp::Ordering;
+
 fn main() {
     println!("Hello, world!");
+}
+//ok
+fn random_num_gen() -> u32 {
+    let randomNum: u32 = rand::thread_rng().gen_range(0..10);
+    return randomNum;
+}
+//working
+fn game_logic() {
+    
 }
